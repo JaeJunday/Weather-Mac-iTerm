67aaae2a00def64580054c03f59a23d3f2b36c78
Jaejun Kim <jaejkim@c2r4s2.42seoul.kr>
2023-07-05 09:48:00 +0900

    style: code style replace

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    system("curl -sSL 'wttr.in/Seoul?m2&lang=ko'");
+    system("curl -sSL 'wttr.in/Seoul?m2&lang=ko' | sed '$d; $d' > .weather && cat .weather");
+    system("rm .weather");
     return 0;
 }
