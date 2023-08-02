34602bef8b5a13ceed42cd645ac4c0acf796011c
JaeJunday <wowns8114@gmail.com>
2023-08-02 09:33:40 +0900

    Style: disk status bar color modified

--- srcs/tools/clear_cache.sh
+++ srcs/tools/clear_cache.sh
@@ -8,6 +8,24 @@ Yellow=$'\033[0;33m'
 Reset=$'\033[0m'
 Cyan=$'\033[0;36m'
 
+BG_BLACK=$'\033[40m'
+BG_RED=$'\033[41m'
+BG_GREEN=$'\033[42m'
+BG_YELLOW=$'\033[43m'
+BG_BLUE=$'\033[44m'
+BG_PURPLE=$'\033[45m'
+BG_CYAN=$'\033[46m'
+BG_WHITE=$'\033[47m'
+B_BG_BLACK=$'\033[100m'
+B_BG_RED=$'\033[101m'
+B_BG_GREEN=$'\033[102m'
+B_BG_YELLOW=$'\033[103m'
+B_BG_BLUE=$'\033[104m'
+B_BG_PURPLE=$'\033[105m'
+B_BG_CYAN=$'\033[106m'
+B_BG_WHITE=$'\033[107m'
+REVERSE=$'\033[7m'
+
 BANNER="$(  
 printf $DELETE
 cat << EOF
@@ -56,13 +74,13 @@ percentage=$(echo "$df_output" | awk '{sub(/%$/, "", $5); print $5}')
 # 컬러 선택
 color=""
 if (( $(awk 'BEGIN {print ('"$percentage"' < 50)}') )); then
-    color=$Cyan  # 초록색
+    color=$B_BG_CYAN  # 초록색
 elif (( $(awk 'BEGIN {print ('"$percentage"' < 65)}') )); then
-    color=$Green  # 초록색
+    color=$B_BG_GREEN  # 초록색
 elif (( $(awk 'BEGIN {print ('"$percentage"' < 80)}') )); then
-    color=$Yellow
+    color=$B_BG_YELLOW
 else
-    color=$Red
+    color=$B_BG_RED
 fi
 
 # df -m 그래프계산값을 위한 정보 출력
@@ -74,19 +92,17 @@ used_g=$(echo "$df_graph" | awk '{print $3}')
 percentage_g=$(echo "$df_graph" | awk '{sub(/%$/, "", $5); print $5}')
 
 # 막대 그래프 생성
-graph="$Yellow[$Reset"
 usage2=$(awk -v total=$total_g -v used=$used_g 'BEGIN {printf "%.0f", used / total * 25}')
 for ((i=0; i<25; i++)); do
     if ((i < usage2)); then
-        graph+="$color#$Reset"
+        graph+="$color.$Reset"
     else
-        graph+="·"
+        graph+="$B_BG_BLACK.$Reset"
     fi
 done
-graph+="$Yellow]$Reset"
 
 # 출력
 printf $DELETE
 printf "$graph "
-printf "$color$percentage\e[0m %% \n" | tr -d "\n"
-curl 'wttr.in/seoul?format=3'
+printf "$Reset$percentage\e[0m %%\n"
+# curl --max-time 3 'wttr.in/seoul?format=3'
